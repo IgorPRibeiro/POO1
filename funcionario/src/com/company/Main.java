014cd2608d84bf74a7d27fbe2c21ff342e24c9be
Igor Pinheiro <igorpinheiroribeiro14@gmail.com>
2021-10-04 19:12:15 -0300

    Contato

--- funcionario/src/com/company/Main.java
+++ funcionario/src/com/company/Main.java
@@ -14,12 +14,17 @@ public class Main {
 
         System.out.println("Nome: ");
         funcionario.setNome(input.nextLine());
+
         System.out.println("Matricula: ");
         funcionario.setMatricula(input.nextInt());
+
         System.out.println("Sario Bruto: ");
         funcionario.setSalarioBruto(input.nextDouble());
+
         System.out.println("Dependentes: ");
         funcionario.setDependente(input.nextInt());
+
+        System.out.println("");
 
         //Show data
         System.out.println("SEUS DADOS:");
@@ -29,14 +34,17 @@ public class Main {
         System.out.println("Sario Bruto: "+ funcionario.salarioBruto);
         System.out.println("Dependentes: "+ funcionario.dependente);
 
+        System.out.println("");
 
         //Show functions
         System.out.println("TOTAL: ");
+
         System.out.printf("Valor do desconto: %.1f\n",funcionario.calcularDesconto());
         System.out.printf("Salario Liquido: %.1f\n", funcionario.calcularSalarioLiquido());
         System.out.printf("Gastos plano de saude: %.1f\n", funcionario.gastosPlanoSaude());
         System.out.printf("Gastos em Vale alimentação: %.1f\n", funcionario.valorValeAlimentacao());
 
+        System.out.println("");
 
         input.close();
 
