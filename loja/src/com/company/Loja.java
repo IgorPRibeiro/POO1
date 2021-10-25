834b2697a5668e98a481f7135055973017e335f5
gccardosin <guiguilacerdacardoso2003@hotmail.com>
2021-10-24 22:18:23 -0300

    Alterações no programa fazendo o mesmo funcionar

--- loja/src/com/company/Loja.java
+++ loja/src/com/company/Loja.java
@@ -5,20 +5,19 @@ public class Loja {
     public Pedido pedido;
     public String produto;
     public Funcionario funcionario;
-//    public void setPedido(Pedido pedido){
-//        this.pedido = pedido;
-//    }
 
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
 
     public String getProduto() {
         return produto;
     }
 
-    public void setProduto(String produto){
+    public void setProduto(String produto) {
 
         this.produto = produto;
     }
-
 
     public void buscarPedido() {
 
@@ -26,20 +25,23 @@ public class Loja {
         String data = pedido.getDataEmissao();
         String cpf = pedido.getCpfCliente();
         String tel = pedido.getTelefoneCliente();
-        System.out.printf("Pedidos : %s\n", pedido_selecionado);
-        System.out.printf("Pedidos : %s\n", data);
-        System.out.printf("Pedidos : %s\n", cpf);
-        System.out.printf("Pedidos : %s\n", tel);
+        if (cpf == null || tel == null) {
+            System.out.printf("Nenhum usuario cadastrado");
+        } else {
+            System.out.printf("Pedidos : %s\n", pedido_selecionado);
+            System.out.printf("Data : %s\n", data);
+            System.out.printf("Cpf : %s\n", cpf);
+            System.out.printf("Tel : %s\n", tel);
+        }
 
     }
 
     public void removerPedido() {
-        funcionario.cadastrarPedido("","");
+        pedido.cadastrarPedido("", "");
     }
 
     public void editarPedido(String pedido_editado, String data) {
-        funcionario.cadastrarPedido(pedido_editado,data);
+        pedido.cadastrarPedido(pedido_editado, data);
     }
-
 
 }
